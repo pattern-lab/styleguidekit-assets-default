02ac014bc826f2f21f6098495992bf5029726672
Brad Frost <brad@bradfrost.com>
2016-05-31 11:26:31 -0400

    Code panels and pattern description styles

--- dist/styleguide/js/patternlab-pattern.js
+++ dist/styleguide/js/patternlab-pattern.js
@@ -300,9 +300,9 @@ window.onpopstate = function (event) {
  */
 
 var panelsUtil = {
-  
+
   addClickEvents: function(templateRendered, patternPartial) {
-    
+
     var els = templateRendered.querySelectorAll('#sg-'+patternPartial+'-tabs li');
     for (var i = 0; i < els.length; ++i) {
       els[i].onclick = function(e) {
@@ -312,42 +312,42 @@ var panelsUtil = {
         panelsUtil.show(patternPartial, panelID);
       };
     }
-    
+
     return templateRendered;
-    
+
   },
-  
+
   show: function(patternPartial, panelID) {
-    
+
     var els;
-    
+
     // turn off all of the active tabs
     els = document.querySelectorAll('#sg-'+patternPartial+'-tabs li');
     for (i = 0; i < els.length; ++i) {
       els[i].classList.remove('sg-tab-title-active');
     }
-    
+
     // hide all of the panels
     els = document.querySelectorAll('#sg-'+patternPartial+'-panels div.sg-tabs-panel');
     for (i = 0; i < els.length; ++i) {
       els[i].style.display = 'none';
     }
-    
+
     // add active tab class
     document.getElementById('sg-'+patternPartial+'-'+panelID+'-tab').classList.add('sg-tab-title-active');
-    
+
     // show the panel
-    document.getElementById('sg-'+patternPartial+'-'+panelID+'-panel').style.display = 'block';
-    
+    document.getElementById('sg-'+patternPartial+'-'+panelID+'-panel').style.display = 'flex';
+
     /*
     if (codeViewer.copyOnInit) {
       codeViewer.selectCode();
       codeViewer.copyOnInit = false;
     }
     */
-    
+
   }
-    
+
 };
 
 /*!
